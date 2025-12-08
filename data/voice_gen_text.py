19e071bbb7177b7fa9dc4a4fcd5c994dfa250fc7
Mac <bellomac16@gmail.com>
2025-12-08 14:23:19 +0800

    Added Trump_show_ori_02

--- data/voice_gen_text.py
+++ data/voice_gen_text.py
@@ -16,4 +16,21 @@ gen_text_list = [
             {'title': 'encourage_betting_12', 'text': '目前莊閒平手，各自補牌決勝負！閒家長龍能否延續？誰能笑到最後？下注時刻，機會就在眼前！'},
         ]
      },
+    {
+        'host': 'Trump', 
+        'context': [
+            {'title': 'encourage_betting_01', 'text': '等等誰先衝起來，這局節奏會很有看頭。'},
+            {'title': 'encourage_betting_02', 'text': '桌上莊門籌碼比較多，要不要再跟就看您了。'},
+            {'title': 'encourage_betting_03', 'text': '壓莊的老闆現在位置相當舒服。'},
+            {'title': 'encourage_betting_04', 'text': '莊家天牌起手，這局莊門優勢超級明顯，要守要衝就看各位老闆心情。'},
+            {'title': 'encourage_betting_05', 'text': '閒家先站中段優勢，莊門氣勢偏弱，壓閒的朋友這局可以好好看戲。'},
+            {'title': 'encourage_betting_06', 'text': '中分對中分，誰也不讓誰，這種拉鋸局，老闆們要拚要休都很合理。'},
+            {'title': 'encourage_betting_07', 'text': '莊家優勢明顯，但閒家長龍氣勢如虹，第三張牌將決定勝負！翻盤的關鍵時刻，快來下注，贏家就是你！'},
+            {'title': 'encourage_betting_08', 'text': '關鍵時刻！閒家能否打破莊家的巨大優勢，延續閒長龍的奇蹟？下注的時刻到了，讓我們拭目以待！'},
+            {'title': 'encourage_betting_09', 'text': '莊家目前以巨大領先，但閒家有長龍加持，會不會逆轉勝呢？各位賭客，這是下注的關鍵時刻，快來押注吧！'},
+            {'title': 'encourage_betting_10', 'text': '莊家巨幅領先，閒家能否打破長龍奇蹟翻盤？下注時刻，機會就在眼前，讓我們拭目以待！'},
+            {'title': 'encourage_betting_11', 'text': '目前牌局緊張刺激！莊家壓住金額領先，究竟誰能逆轉勝局？快來下注，見證奇蹟時刻！'},
+            {'title': 'encourage_betting_12', 'text': '目前莊閒平手，各自補牌決勝負！閒家長龍能否延續？誰能笑到最後？下注時刻，機會就在眼前！'},
+        ]
+     },
 ]
